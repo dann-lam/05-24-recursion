13e56b9a32d5fdec449456ae27b769ae108f49b3
Daniel Lam <Dann.lam@gmail.com>
2022-05-24 14:48:48 -0700

    13

--- problems/13-sort.js
+++ problems/13-sort.js
@@ -22,8 +22,50 @@ sort([]); // []
 ***********************************************************************/
 
 function sort(nums, sorted = []) {
-  // your code here
+if(!nums.length) return sorted;
+
+let smallest = Infinity;
+
+for(let i = 0; i < nums.length; i++){
+  if (smallest > nums[i]){
+    smallest = nums[i]
+  }
 }
+
+let index = nums.indexOf(smallest)
+nums.splice(index, 1)
+
+sorted.push(smallest)
+
+return sort(nums, sorted)
+}
+
+
+// if(!nums.length) return sorted;
+
+// let smallest = 0;
+
+// for(let i = 1; i < nums.length; i++){
+//   if (nums[smallest] > nums[i]){
+//     console.log("nums smallest is " + nums[smallest])
+//     console.log("nums i is " + nums[i])
+//     smallest = i
+//     //sm 1,
+//     //sm = 0, i = 1
+//   }
+// }
+
+// let index = nums.indexOf(smallest)
+// nums.splice(index, 1)
+// console.log("My nums splice is " +   nums.splice(index, 1))
+// sorted.push(nums[index])
+// console.log("my sorted push is " +   sorted.push(nums[smallest]))
+// return sort(nums, sorted)
+// sort([4,1,6,3,1,7]); // [1, 1, 3, 4, 6, 7]
+// console.log(sort([4,1,6,3,1,7])); // [1, 1, 3, 4, 6, 7])
+// console.log(sort([0, 1, -3])); // [-3, 0, 1]
+// console.log(sort([])); // []
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
--- test/13-sort-spec.js
+++ test/13-sort-spec.js
@@ -6,6 +6,7 @@ describe("sort()", function () {
   it("should sort an array of numbers from least to greatest", function () {
     assert.deepEqual(sort([4, 1, 6, 3, 1, 7]), [1, 1, 3, 4, 6, 7]);
     assert.deepEqual(sort([0, 1, -3]), [-3, 0, 1]);
+    assert.deepEqual(sort([0, 1]), [0, 1]);
     assert.deepEqual(sort([]), []);
     assert.deepEqual(sort([-5.5, -10, 100, 0]), [-10, -5.5, 0, 100]);
     assert.deepEqual(sort([5, 2, 18, 6, 8]), [2, 5, 6, 8, 18]);
